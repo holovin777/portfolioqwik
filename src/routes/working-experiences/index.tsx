285f62028a904cf8218e53eef00b3f2d9cf66bba
Viktor Holovin <holovin@mail.com>
2023-04-25 14:32:36 +0200

    Card component in working experiences

--- src/routes/working-experiences/index.tsx
+++ src/routes/working-experiences/index.tsx
@@ -1,5 +1,6 @@
 import { component$ } from '@builder.io/qwik';
 import { routeLoader$ } from '@builder.io/qwik-city';
+import Card from '~/components/card/card';
 import Header from '~/components/header/header';
 
 
@@ -30,28 +31,7 @@ export default component$(() => {
             <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3">
                 {
                     customerWorkingExperiences.value.map((we) =>
-                        <div key={we.id} class="bg-blue-300 bg-opacity-50 text-indigo-900 dark:bg-blue-950 dark:bg-opacity-50 rounded-lg m-6 p-2 dark:text-indigo-300">
-                            {
-                                !(we.finishedWork === null) &&
-                                <div class="text-xs text-center text-yellow-300 m-1">
-                                    Finished work {we.finishedWork}
-                                </div>
-                            }
-                            <div class="">
-                                <div class="text-xl font-bold my-1 mx-3">
-                                    {we.positionAtWork.name}
-                                </div>
-                                <div class="text-gray-700 dark:text-gray-400 my-1 mx-6">
-                                    {we.company.name}
-                                </div>
-                                <div class="text-xs text-right text-gray-600 dark:text-gray-500 my-2">
-                                    {we.company.location}
-                                </div>
-                            </div>
-                            <div class="text-xs text-center text-yellow-300 my-2">
-                                Started work {we.startedWork}
-                            </div>
-                        </div>
+                        <Card key={we.id} title={we.positionAtWork.name} subtitle={we.company.name} startDate={we.startedWork} finishDate={we.finishedWork} footer={we.company.location} />
                     )
                 }
             </div>
