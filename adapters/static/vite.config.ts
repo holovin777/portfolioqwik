d6899f96e382868322c34ce27261e2f8a55ac469
Viktor Holovin <holovin@mail.com>
2023-09-10 12:58:35 +0200

    Create site link for ssg

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -12,7 +12,7 @@ export default extendConfig(baseConfig, () => {
     },
     plugins: [
       staticAdapter({
-        origin: "https://holovin.com",
+        origin: "https://curriculum.holovin.com",
       }),
     ],
   };
