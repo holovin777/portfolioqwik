9b29c9a4f9a4c2af8a9d55dc4e136120fd303b86
Viktor Holovin <holovin@mail.com>
2023-09-18 17:58:40 +0200

    Change link for customer

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -12,7 +12,7 @@ export default extendConfig(baseConfig, () => {
     },
     plugins: [
       staticAdapter({
-        origin: "https://curriculum.holovin.com",
+        origin: ("https://www.holovin.com"),
       }),
     ],
   };
